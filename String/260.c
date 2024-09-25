e206526d0d907f1229f56ad4eb28b8ccc910dae2
richardlaiis <richardlaiis@proton.me>
2024-09-25 22:03:02 +0800

    刷單班的水題XD

--- String/260.c
+++ String/260.c
@@ -8,9 +8,35 @@ char* substr(int start, int end, char string[]) {
 	dest[cnt] = '\0';
 	return dest;
 }
+int min(int x, int y) {
+	return (x<y)?x:y;
+}
 int main() {
-	char hello[] = "Hello, world!";
-	char *result = substr(3, 8, hello);
-	printf("%s\n", result);
-	free(result);
+	//char hello[] = "Hello, world!";
+	//char *result = substr(3, 8, hello);
+	//printf("%s\n", result);
+	//free(result);
+	
+	char *ans = "";
+	char input[128];
+	int n = strlen(ans);
+	while(scanf("%s",&input)!=EOF) {
+		int len = strlen(input);
+		if(n==0) {
+			ans = input;
+			n = strlen(ans);
+		}else {
+			int cnt = 0;
+			for(int i = 0; i < min(n, len); i++) {
+				char *result1 = substr(0,i,input);
+				char *result2 = substr(n,n-i,ans);
+				if(result1==result2) cnt++;
+				else break;
+			}
+			if(cnt==0) strcat(ans, input);
+			else strncat(ans, input+len-cnt, cnt);
+			n = strlen(ans);
+		}
+	}
+	printf("%s\n", ans);
 }
