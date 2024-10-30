6d82af4bef287f4d68be0910a72d74df9aa0fabf
richardlaiis <richardlaiis@proton.me>
2024-10-30 15:12:30 +0800

    SQL problem is hard QwQ

--- Structure/210_SQL_Database.c
+++ Structure/210_SQL_Database.c
@@ -1,42 +1,123 @@
 #include <stdio.h>
 #include <string.h>
-#include <stdbool.h>
-#define maxn 500
-typedef struct Data {
-	char lastname[85],firstname[85],id[85];
+#define S_MAX 81
+#define MAXN 50
+typedef struct data {
+	char lastname[S_MAX], firstname[S_MAX], id[S_MAX];
 	int salary, age;
-} data;
-int stoi(char *s) {
+} Data;
+int convertInt(char s[]) {
 	int num = 0;
 	int len = strlen(s);
-	for(int i = 0; i < len; i++) {
-		num = num*10+(s[i]-'0');
-	}
+	for(int i = 0; i < len; i++)
+		num = num*10 + (s[i]-'0');
 	return num;
-}	
-int main() {
-	int n;
-	scanf("%d", &n);
-	data db[n];
+}
+void print(Data a, char field[8][10], int n) {
 	for(int i = 0; i < n; i++) {
-		scanf("%s%s%s%d%d", &(db[i].lastname), &(db[i].firstname), &(db[i].id), &(db[i].salary), &(db[i].age));
+		if(field[i][0] == 'l') printf("%s%c",a.lastname," \n"[i == n-1]);
+		else if(field[i][0] == 'f') printf("%s%c",a.firstname," \n"[i == n-1]);
+		else if(field[i][0] == 'I') printf("%s%c",a.id," \n"[i == n-1]);
+		else if(field[i][0] == 's') printf("%d%c",a.salary," \n"[i == n-1]);
+		else printf("%d%c", a.age, " \n"[i == n-1]);
 	}
-	int k;
-	scanf("%d", &k);
-	char query[maxn];
-	while(fgets(query, maxn, stdin)) {
-		char token[20][85];
-		char *start = strtok(query, " ");
-		int idx = 0;
-		while(start != NULL) {
-			strcpy(token[idx++], start);
-			start = strtok(NULL, " ");
-		}
-		if(strcmp("salary", token[idx-3]) == 0) {
-			int wage = stoi(token[idx-1]);
-			if(strcmp("==", token[idx-2]) == 0) {
-				
+}
+void processNum(char f[10], char op[3], int x, Data database[], char fs[9][10], int m, int n) {
+	if(f[0] == 's') {
+		if(op[0] == '=') {
+			for(int i = 0; i < n; i++) {
+				if(database[i].salary == x) print(database[i], fs, m);
+			}
+		}else if(op[0] == '<') {
+			for(int i = 0; i < n; i++) {
+				if(database[i].salary < x) print(database[i], fs, m);
+			}
+		}else {
+			for(int i = 0; i < n; i++) {
+				if(database[i].salary > x) print(database[i], fs, m);
 			}
 		}
+	}else {
+		if(op[0] == '=') {
+			for(int i = 0; i < n; i++) {
+				if(database[i].age == x) print(database[i], fs, m);
+			}
+		}else if(op[0] == '<') {
+			for(int i = 0; i < n; i++) {
+				if(database[i].age < x) print(database[i], fs, m);
+			}
+		}else {
+			for(int i = 0; i < n; i++) {
+				if(database[i].age > x) print(database[i], fs, m);
+			}
+		}
+	}
+}
+void processStr(char f[10], char op[3], char cons[], Data database[], char fs[9][10], int m, int n) {
+	if(f[0] == 'f') {
+		if(op[0] == '=') {
+			for(int i = 0; i < n; i++) {
+				if(strcmp(cons, database[i].firstname) == 0)
+					print(database[i], fs, m);
+			}
+		}else {
+			for(int i = 0; i < n; i++) {
+				if(strcmp(cons, database[i].firstname) != 0)
+					print(database[i], fs, m);
+			}
+		}
+	}else if(f[0] == 'l') {
+
+		if(op[0] == '=') {
+			for(int i = 0; i < n; i++) {
+				if(strcmp(cons, database[i].lastname) == 0)
+					print(database[i], fs, m);
+			}
+		}else {
+			for(int i = 0; i < n; i++) {
+				if(strcmp(cons, database[i].lastname) != 0)
+					print(database[i], fs, m);
+			}
+		}
+	}else if(f[0] == 'I') {
+
+		if(op[0] == '=') {
+			for(int i = 0; i < n; i++) {
+				if(strcmp(cons, database[i].id) == 0)
+					print(database[i], fs, m);
+			}
+		}else {
+			for(int i = 0; i < n; i++) {
+				if(strcmp(cons, database[i].id) != 0)
+					print(database[i], fs, m);
+			}
+		}
+	}
+}
+int main() {
+	Data database[MAXN];
+	int n, q;
+	scanf("%d", &n);
+	for(int i = 0; i < n; i++)
+		scanf("%s%s%s%d%d", database[i].lastname, database[i].firstname, database[i].id, &database[i].salary, &database[i].age);
+	scanf("%d", &q);
+	for(int i = 0; i < q; i++) {
+		char tmp[7];
+		scanf("%s", tmp);
+		char field[9][10];
+		int idx = 0;
+		while(scanf("%s", field[idx]) && field[idx][0] != 'w')
+			idx++;
+		
+		char f[10], op[3], con[S_MAX];
+		scanf("%s%s%s", f, op, con);
+		
+		if(f[0] == 's' || f[0] == 'a') {
+			int x = convertInt(con);
+			processNum(f, op, x, database, field, idx, n);
+		}else {
+			processStr(f, op, con, database, field, idx, n);
+		}
+
 	}
 }
