d210f917cbc2e8e99a032cf8e6e55dde1c67dda0
richardlaiis <richardlaiis@proton.me>
2024-11-04 16:13:18 +0800

    be like shit

--- Recursion/261.c
+++ Recursion/261.c
@@ -1,17 +1,39 @@
 #include <stdio.h>
+#include <assert.h>
 #include <string.h>
+#include <stdbool.h>
 #include <ctype.h>
-int convertInt(char s[20]) {
+#define maxn 9
+int convertInt(char s[]) {
+	int n = strlen(s);
 	int num = 0;
-	int len = strlen(s);
-	for(int i = 0; i < len; i++)
+	for(int i = 0; i < n; i++) {
 		num = num*10 + (s[i]-'0');
+	}
 	return num;
 }
-void solve(char s1[], char s2[], char result[], int idx1, int idx2) {
-}
+
 int main() {
-	char s1[9], s2[9], result[12];
-	scanf("%s%s%s", s1, s2, result);
-	solve(s1, s2, result, 0, 0);
+	char s1[maxn], s2[maxn], s3[maxn];
+	assert(scanf("%s%s%s", s1, s2, s3) == 3);
+	bool alpha[26] = {0};
+	int l1 = strlen(s1);
+	for(int i = 0 ;i < l1; i++) {
+		if(!isdigit(s1[i])) {
+			alpha[s1[i]-'A'] = 1;
+		}
+	}
+	int l2 = strlen(s2);
+	for(int i = 0 ;i < l2; i++) {
+		if(!isdigit(s2[i])) {
+			alpha[s2[i]-'A'] = 1;
+		}
+	}
+	int l3 = strlen(s3);
+	for(int i = 0 ;i < l3; i++) {
+		if(!isdigit(s3[i])) {
+			alpha[s3[i]-'A'] = 1;
+		}
+	}
+	int a[26];
 }
