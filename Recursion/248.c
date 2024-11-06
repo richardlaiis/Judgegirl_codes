b5ddc0f7c42dbb91e2dcbf51834b35afd17e0b89
richardlaiis <richardlaiis@proton.me>
2024-11-06 15:08:52 +0800

    owo

---
+++ Recursion/248.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#define SIZE 9
+int main() {
+    int cnt[SIZE][SIZE];
+    for(int i = 0; i < SIZE; i++) {
+        for(int j = 0; j < SIZE; j++) {
+            scanf("%d", &cnt[i][j]);
+        }
+    }
+    
+}
